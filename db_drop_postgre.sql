23736362177858e12c4b02dd431724dfe1b09e5d
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2024-01-11 15:52:48 +0000

    Add files via upload

---
+++ db_drop_postgre.sql
@@ -0,0 +1,14 @@
+drop table public.constrparam;
+drop table public.constrinfo;
+drop table public."constraint";
+drop table public.constrtype;
+drop table public.objparam;
+drop table public.object;
+drop table public.objtype;
+drop table public.param;
+drop table public.entity;
+drop table public.sketch;
+drop table public.plane;
+drop table public.access_list;
+drop table public.model;
+drop table public.user;
